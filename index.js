3a69788892a7c99d008357fb78238a3be36b1d98
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-15 22:39:53 +0300

    modifies index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b ) {
+    return a + b   
+  }
+function subtract(a, b ) {
+    return a - b   
+  }
+function multiply(a, b ) {
+    return a*b   
+  }
+function divide(a, b ) {
+    return a/b   
+  }
+  function divide(a, b ) {
+    return a/b   
+  }
+  function increment(n) {
+    return ++n;
+}
+console.log(increment(1));
+function decrement(n) {
+    return --n;
+}
+console.log(decrement(4));
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function  preserveDecimal(n){
+    return parseFloat(n);
+}
